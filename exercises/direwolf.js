64e773b06a84038f2a45348437899da1f999f226
dawnlunacy <alanturing@example.com>
2019-05-28 16:56:55 -0600

    erase psuedocode

--- exercises/direwolf.js
+++ exercises/direwolf.js
@@ -4,7 +4,6 @@ class Direwolf {
     this.home = home || 'Beyond the Wall';
     this.size = size || 'Massive';
     this.starksToProtect = [];
-    // this.counter = 0;
     this.huntsWhiteWalkers = true;
     
   };
@@ -14,22 +13,14 @@ class Direwolf {
        this.starksToProtect.push(stark);
        stark.safe = true;
        this.huntsWhiteWalkers = false;
-    } 
     };
+  };
 
     leave(stark) {
       this.starksToProtect = []
       stark.safe = false;
     }
     
-  //   } else if (this.counter <= 2) {
-  //   this.starksToProtect.push(stark)
-  //   } else (this.counter > 2){
-  //     this.counter = 0
-  //     this.starksToProtect = [];
-  //     this.starksToProtect.push(stark)
-  //   };
-  // };
 };
 
 module.exports = Direwolf;
